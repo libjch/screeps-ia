2fd74c0c6f85550d39bbb1eaa3a87f368836ae8e
JC Libbrecht <jc.libbrecht@takeeateasy.be>
2016-07-17 08:41:09 +0200

    extractors usage

--- src/direction.util.js
+++ src/direction.util.js
@@ -89,7 +89,7 @@ function findSourceInRoom(creep){
     }
     logger.info('No sources from exractor');
 
-    var sources = creep.room.find(FIND_SOURCES,{filter: (source) => { return source.energy > 0 && Memory.extractors[source.id].creep == undefined}});
+    var sources = creep.room.find(FIND_SOURCES,{filter: (source) => { return source.energy > 0}});//Memory.extractors[source.id].creep == undefined}});
     if(sources.length){
         var sourceNumber = creep.memory.number % sources.length;
         var source = sources[sourceNumber];
