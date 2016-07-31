61298befe200d7af17a6e1854d2f03921016af94
JC Libbrecht <jc.libbrecht@takeeateasy.be>
2016-07-31 13:47:07 +0200

    more attackers

--- src/global.variables.js
+++ src/global.variables.js
@@ -14,13 +14,13 @@ var truc = {
         data['others'] = {};
         (data['others'])['E42S38'] = ['E41S38','E42S39'];
         (data['others'])['E43S38'] = ['E43S37'];
-        (data['others'])['E44S38'] = ['E44S39'];
+        (data['others'])['E44S38'] = ['E44S39','E45S38','E44S37'];
         //data['targets_path'] = ['E43S38','E43S37','E44S37','E44S36','E43S36','E42S36'];
         //data['targets_final'] ='E42S36';
         data['targets_path'] = ['E43S38','E44S38','E44S37'];
         data['targets_final'] ='E44S37';
 
-        data['attacker'] = true;
+        data['attacker'] = false;
         return data;
     }
 };
