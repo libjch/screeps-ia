644f1adf651945629fe85c56bd41bb40b724dd69
JC Libbrecht <jc.libbrecht@takeeateasy.be>
2016-11-04 15:37:59 +0100

    restart

--- src/main.js
+++ src/main.js
@@ -21,7 +21,6 @@ var logger = require('logger');
 
 module.exports.loop = function () {
     logger.highlight('========== NEW TURN ============',1);
-    //Clear: Game.rooms['E42S38'].find(FIND_CONSTRUCTION_SITES).forEach(a => a.remove());
 
     for(var i in Memory.creeps) {
         if(!Game.creeps[i]) {
@@ -29,8 +28,8 @@ module.exports.loop = function () {
         }
     }
     spawnDecider.spawn();
+    //towerAttack.attack();
 
-    towerAttack.attack();
     roleExtractor.cleanExtractors();
 
     var creeps = [];
@@ -54,44 +53,26 @@ module.exports.loop = function () {
     for(let creep of creeps) {
 
         //pickup dropped enery;
-        if (creep.carry.energy < creep.carryCapacity)
-        {
+        if (creep.carry.energy < creep.carryCapacity) {
             var energy = creep.pos.findInRange(FIND_DROPPED_ENERGY,1);
             if (energy.length) {
-                console.log('found ' + energy[0].energy + ' energy at ', energy[0].pos);
-                creep.pickup(energy[0]);
+                for(let e of energy) {
+                    creep.pickup(e);
+                }
             }
-        }
-        //if(creep.ticksToLive < 300){
-        //    creep.memory.extern = false;
-        //}
-        if(creep.memory.roomnumber){
-            creep.memory.externRoom = creep.memory.roomnumber;
         }
 
         var place = creep.room.name+'-'+creep.pos.x+'-'+creep.pos.y;
-        if(!Memory.roadPlaces[place]){
-            Memory.roadPlaces[place]=1;
-        }else{
-            Memory.roadPlaces[place] = Memory.roadPlaces[place]+1;
-        }
-
-        /*if(creep.memory.extern && !creep.memory.externRoom ){
-            var otherRooms = constants.rooms().others[creep.memory.mainroom];
-            var x = creep.memory.number % otherRooms.length;
-            creep.memory.externRoom = x;
-        }*/
-
-        logger.warn('-'+creep.name+' '+creep.memory.mainroom+' '+
-            (creep.memory.extern?'(E '+creep.memory.externRoom+') ' : '')+
-            creep.memory.number + ' ' +
-            creep.memory.role +' '+
-            creep.carry.energy +'/'+creep.carryCapacity +' ' +
-            creep.pos);
 
         //run roles
         try{
             var role = creep.memory.role;
+
+            if(role == undefined){
+
+            }
+
+
             if(creep.memory.role_override){
                 role = creep.memory.role_override;
             }
