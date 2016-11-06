91e58d0136da5002ed028274b7f526c5c4bdd0cc
JC Libbrecht <jc.libbrecht@takeeateasy.be>
2016-11-06 15:22:09 +0100

    Fix tower spawn

--- src/wall.planner.js
+++ src/wall.planner.js
@@ -20,10 +20,23 @@ module.exports = {
                 if (room.controller && room.controller.my) {
                     //var exits = Game.map.describeExits(roomName);
 
+                    var spawn = room.find(FIND_MY_STRUCTURES, {filter: (s) => s.structureType == STRUCTURE_SPAWN});
+
+                    var exit = creep.pos.findClosestByPath(FIND_EXIT_LEFT);
+                    logger.debug(exit);
+
+                    var ignoreList = {};
                     //Left wall:
                     var entrances = room.lookForAtArea(LOOK_TERRAIN,0,0,49,0);
+                    var x = 0;
                     for(let y = 0;y<49;y++){
-                        logger.debug(entrances[y][0]);
+                        if(entrances[y][0] == 'plain'){
+
+                            //var res = room.createConstructionSite(x,y,STRUCTURE_WALL);
+                            if(res != ERR_INVALID_TARGET){
+                                logger.debug("Create wall:" +res ,classname);
+                            }
+                        }
                     }
                 }
             }
