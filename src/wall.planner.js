ead17c4345db01327a92cd28f8a28002e64b73ec
JC Libbrecht <jc.libbrecht@takeeateasy.be>
2016-11-06 15:27:45 +0100

    Fix tower spawn

--- src/wall.planner.js
+++ src/wall.planner.js
@@ -21,7 +21,7 @@ module.exports = {
                     //var exits = Game.map.describeExits(roomName);
 
                     var spawn = room.find(FIND_MY_STRUCTURES, {filter: (s) => s.structureType == STRUCTURE_SPAWN});
-                    logger.debug(spawn+" "+spawn.pos);
+                    logger.debug(spawn+" "+Game.spawns[spawn].pos);
 
                     var exit = spawn.pos.findClosestByPath(FIND_EXIT_LEFT);
                     logger.debug(exit);
