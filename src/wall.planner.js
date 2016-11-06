d3c3cfd1e4b4194349c6f4e4d14adc085f271e96
JC Libbrecht <jc.libbrecht@takeeateasy.be>
2016-11-06 15:33:24 +0100

    Wall Spawn

--- src/wall.planner.js
+++ src/wall.planner.js
@@ -27,16 +27,22 @@ module.exports = {
                     var exit = spawn.pos.findClosestByPath(FIND_EXIT_LEFT);
                     logger.debug(exit);
 
-                    var ignoreList = {};
                     //Left wall:
                     var entrances = room.lookForAtArea(LOOK_TERRAIN,0,0,49,0);
                     var x = 0;
                     for(let y = 0;y<49;y++){
                         if(entrances[y][0] == 'plain'){
 
-                            //var res = room.createConstructionSite(x,y,STRUCTURE_WALL);
-                            if(res != ERR_INVALID_TARGET){
-                                logger.debug("Create wall:" +res ,classname);
+                            if(y == exit.y){
+                                var res = room.createConstructionSite(x,y,STRUCTURE_RAMPART);
+                                if(res != ERR_INVALID_TARGET){
+                                    logger.debug("Create Rampart:" +res ,classname);
+                                }
+                            }else{
+                                var res = room.createConstructionSite(x,y,STRUCTURE_WALL);
+                                if(res != ERR_INVALID_TARGET){
+                                    logger.debug("Create wall:" +res ,classname);
+                                }
                             }
                         }
                     }
