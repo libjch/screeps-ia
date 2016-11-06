2ce6b93e7caad432443a32677ffeb9e2b9b072aa
JC Libbrecht <jc.libbrecht@takeeateasy.be>
2016-11-06 14:45:07 +0100

    Fix tower spawn

--- src/stats.record.js
+++ src/stats.record.js
@@ -17,8 +17,8 @@ function record() {
 
 
     for(var room of rooms){
-        logger.debug(room+' '+room.name,classname);
-        if(room && room.mode != MODE_SIMULATION){
+        logger.debug(room+' '+room.name+" "+room.mode,classname);
+        if(room && (room.mode != MODE_SIMULATION)){
             if(room.controller.my){
                 Memory.stats["room." + room.name + ".controllerProgress"] = room.controller.progress;
                 var roomCreeps = _.filter(Game.creeps, (creep) => creep.memory.mainroom == room.name);
