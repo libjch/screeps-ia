c76260799cb039028b92c7a05b3a5c85a2617623
JC Libbrecht <jc.libbrecht@takeeateasy.be>
2016-11-07 10:59:30 +0100

    Fix attacker ratio

--- src/spawn.decide.js
+++ src/spawn.decide.js
@@ -132,14 +132,12 @@ module.exports = {
                     body.push(CARRY);
                 }else if (role == 'attacker') {
                     var number = Math.floor(maxEnergy / 250);
-                    var rest = maxEnergy % 250;
+                    var rest = maxEnergy % 190;
                     number -= 1;
                     for (var i = 0; i < number; i++) {
                         body.push(TOUGH);
-                        body.push(TOUGH);
                     }
                     for (var i = 0; i < number; i++) {
-                        body.push(MOVE);
                         body.push(MOVE);
                         body.push(MOVE);
                     }
