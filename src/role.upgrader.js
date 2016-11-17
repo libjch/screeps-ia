7b4c8407e05760927b8826fbda98e0bf52954203
JC Libbrecht <jc.libbrecht@takeeateasy.be>
2016-11-17 18:29:43 +0100

    optimize upgrader

--- src/role.upgrader.js
+++ src/role.upgrader.js
@@ -1,7 +1,6 @@
 /**
  * Created by libjch on 12/07/16.
  */
-var constants = require('global.variables');
 var direction = require('util.direction');
 var logger = require('logger');
 var classname = 'RoleUpgrader';
@@ -14,7 +13,6 @@ var roleUpgrader = {
         if(creep.memory.working && creep.carry.energy == 0) {
             creep.memory.working = false;
         }
-        //logger.log("Upgrading: "+(creep.carry.energy == creep.carryCapacity)+" "+creep.carry.energy +"/"+creep.carryCapacity,classname);
         if(!creep.memory.working && creep.carry.energy == creep.carryCapacity) {
             creep.memory.working = true;
         }
@@ -22,32 +20,20 @@ var roleUpgrader = {
         if(creep.memory.working) {
             if(creep.room.controller.my && creep.memory.mainroom == creep.room.name){
                 if(creep.upgradeController(creep.room.controller) == ERR_NOT_IN_RANGE) {
-                    creep.moveTo(creep.room.controller);
+                    creep.moveToFatigue(creep.room.controller);
                 }
             }else{
-                //NOT in current room
                 direction.moveToRoom(creep,creep.memory.mainroom);
             }
         }
         else {
-            var targets = creep.room.find(FIND_STRUCTURES, {
-                filter: (structure) => {
-                    return (structure.structureType == STRUCTURE_STORAGE && structure.store[RESOURCE_ENERGY] >= 50000)
-                }
-            });
-            var target = targets[0];
-            if(target){
-                if(target.transfer(creep,RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
-                    creep.moveTo(target);
+            if(creep.room.storage && creep.room.storage.store[RESOURCE_ENERGY] >= 50000) {
+                if (creep.withdraw(creep.room.storage, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
+                    creep.moveToFatigue(creep.room.storage);
                 }
                 return;
             }
-
-            if(creep.memory.extern && creep.room.name == creep.memory.mainroom){
-                direction.moveToRoom(creep,constants.rooms().others[creep.memory.mainroom][creep.memory.externRoom]);
-            }else{
-                direction.findSourceInRoom(creep);
-            }
+            direction.findSourceInRoom(creep);
         }
     }
 };
