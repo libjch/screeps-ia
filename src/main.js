b1e8b0648934606107d918aa2e4874fed8f0b610
JC Libbrecht <jc.libbrecht@takeeateasy.be>
2016-11-18 15:29:59 +0100

    fix restart

--- src/main.js
+++ src/main.js
@@ -43,8 +43,6 @@ module.exports.loop = function () {
     logger.trace('CPU usage:' + Game.cpu.getUsed() + ' tickLimit:' + Game.cpu.tickLimit + ' bucket:' + Game.cpu.bucket + ' limit:' + Game.cpu.limit);
     logger.info(Memory.start);
 
-    return;
-
     logger.init();
 
     tick('Memory Loaded');
