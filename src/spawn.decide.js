cb4a1694e5e3aceb839b8e8bfcd3e45503f8fe06
JC Libbrecht <jc.libbrecht@takeeateasy.be>
2016-11-20 23:36:53 +0100

    builderhelpers

--- src/spawn.decide.js
+++ src/spawn.decide.js
@@ -90,6 +90,10 @@ module.exports = {
             else if (constructionsSites.length / 11 > builders.length && builders.length < 1) {
                 role = 'builder';
                 maxEnergy = maxEnergy > 1200 ? 1200 : maxEnergy;
+            } else if (buildersHelpers.length < 1 && (roomName == 'W68S31') ) { //+1
+                role = 'builder';
+                maxEnergy = maxEnergy > 1500 ? 1500 : maxEnergy;
+                mainroom = 'W68S31';
             } else if (repairers.length < 1) {
                 role = 'repairer';
                 maxEnergy = maxEnergy > 1200 ? 1200 : maxEnergy;
