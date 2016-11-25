71f867027e30990fd85c6079bba7b5539cdcd4d0
JC Libbrecht <jc.libbrecht@takeeateasy.be>
2016-11-25 09:28:10 +0100

    optimized TowerAttack

--- src/tower.attack.js
+++ src/tower.attack.js
@@ -5,20 +5,36 @@ var classname = 'TowerAttack';
 
 
 Room.prototype.runTowers = function () {
-    var ts = this.find(FIND_MY_STRUCTURES, {
+    //List towers
+    var towers = this.find(FIND_MY_STRUCTURES, {
         filter: (s) => s.structureType == STRUCTURE_TOWER
     });
-    for(let t of ts){
-        t.work();
+    //1 find hostiles:
+    var hostiles = this.find(FIND_HOSTILE_CREEPS);
+
+    if(hostiles && hostiles.length > 0){
+        for(let t of towers){
+            t.workAttack(hostiles);
+        }
+    }else{
+        var repairsTargets = this.find(FIND_STRUCTURES, {
+            filter: (structure) => {
+                return (structure.structureType == STRUCTURE_ROAD && structure.hits > 0 && structure.hits < (structure.hitsMax * 0.2));
+            }
+        });
+        if(repairsTargets && repairsTargets.length > 0){
+            for(let t of towers){
+                t.workRepair(repairsTargets);
+            }
+        }
     }
 }
 
-StructureTower.prototype.work = function(){
+StructureTower.prototype.workAttack = function(targets){
     logger.info('Tower : '+this.pos,classname);
-    var closestHostile = this.pos.findClosestByRange(FIND_HOSTILE_CREEPS);
+    var closestHostile = this.pos.findClosestByRange(targets);
     if(closestHostile) {
         this.attack(closestHostile);
-        //return;
     }else{
         for(var i in Game.creeps) {
             var creep = Game.creeps[i];
@@ -28,22 +44,12 @@ StructureTower.prototype.work = function(){
             }
         }
         this.workRepair();
-        //return;
     }
 }
 
 
-StructureTower.prototype.workRepair = function(){
-    var targets = [];
-    var targetRoads = this.room.find(FIND_STRUCTURES, {
-        filter: (structure) => {
-            return (structure.structureType == STRUCTURE_ROAD && structure.hits > 0 && structure.hits < (structure.hitsMax * 0.2));
-        }
-    });
-
+StructureTower.prototype.workRepair = function(targets){
     logger.info('Targets '+targets,classname);
-    targets = targetRoads;
-
     if(targets.length){
         this.repair(targets[0]);
     }
