bcd3be92bf9b495352a07fa0cd92c53afee4490c
JC Libbrecht <jc.libbrecht@takeeateasy.be>
2017-03-23 23:18:34 +0100

    attaaack

--- src/role.attacker.js
+++ src/role.attacker.js
@@ -84,6 +84,7 @@ Creep.prototype.workAttack = function(){
     }
 
     if(Game.flags['attack-target-1']){
+        logger.warn("FLAG FOUND!" +Game.flags['attack-target-1']);
         if(this.room.name == Game.flags['attack-target-1'].room.name){
             var targets = this.room.lookAt(Game.flags['attack-target-1'].pos.x,Game.flags['attack-target-1'].pos.y);
             if(targets.length){
