28c188863977f684bafb02c9cc613efcdb28e051
JC Libbrecht <libjch@gmail.com>
2018-10-15 22:47:11 +0200

    double spawner target

--- src/spawn.decide.js
+++ src/spawn.decide.js
@@ -104,10 +104,14 @@ Room.prototype.spawnDecide = function(){
         } else if (constructionsSites.length / 11 > builders.length && builders.length < 1 && Game.cpu.bucket > 2500) {
             role = 'builder';
             maxEnergy = maxEnergy > (1200 * maxEnergyRatio)  ? (1200 * maxEnergyRatio) : maxEnergy;
-        } else if (this.controller.level >= 3 && Memory.spawner.target && buildersHelpers.length < 2) { //+1
+        } else if (this.controller.level >= 5 && Memory.spawner.target && buildersHelpers.length < 2) { //+1
             role = 'builder';
-            maxEnergy = maxEnergy > 3000 ? 3000 : maxEnergy;
+            maxEnergy = maxEnergy > 1000 ? 1000 : maxEnergy;
             mainroom = Memory.spawner.target;
+        } else if (this.controller.level >= 5 && Memory.spawner.target2 && buildersHelpers.length < 2) { //+1
+            role = 'builder';
+            maxEnergy = maxEnergy > 1000 ? 1000 : maxEnergy;
+            mainroom = Memory.spawner.target2;
         } else if (repairers.length < 1 && Game.cpu.bucket > 2500) {
             role = 'repairer';
             maxEnergy = maxEnergy > 1200 ? 1200 : maxEnergy;
